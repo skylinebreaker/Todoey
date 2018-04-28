2f0c8c03943263421f25772709b630a39fdb0efe
Bowen Shen <bowenshe@usc.edu>
2018-04-27 22:43:29 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Bowen Shen on 4/27/18.
+//  Copyright © 2018 Bowen Shen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
